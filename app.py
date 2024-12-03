302d9219c19641aa9548a08c56dcf2cbdd18e18f
Jaewon Lee <joyjwlee@gmail.com>
2024-12-02 21:44:15 -0800

    add time as a factor

--- app.py
+++ app.py
@@ -5,11 +5,20 @@ from shapely.geometry import Point
 import folium
 from streamlit_folium import st_folium
 import branca.colormap as cm 
+import pandas as pd
+
 # class
 from utilities import TimePrioritizer, MoneyPrioritizer, SafetyPrioritizer, Population, TransportCost
 # constants 
 latitude_miles_per_degree = 69  
-longitude_miles_per_degree = 55.2  
+longitude_miles_per_degree = 55.2
+
+# average incidents by hour
+incidents = pd.read_csv("./incidents.csv")
+incidents.set_index('Hour', inplace=True)
+incidents = incidents.T
+incidents_mean = incidents.mean()
+scaling_factor = 1 + (incidents_mean - incidents_mean.min()) / (incidents_mean.max() - incidents_mean.min())
 
 # population = Population(1/3,1/3,1/3)
 # get the cost of uber all way 
@@ -46,6 +55,7 @@ safety_cost = st.slider("Safety Cost ($)", 10, 50, 20)
 st.write("### Adjust Population Percentages")
 time_pc = st.slider("Time Prioritizer (%)", 0.0, 1.0, 1 / 3, step=0.01)
 money_pc = st.slider("Money Prioritizer (%)", 0.0, 1.0, 1 / 3, step=0.01)
+hour_of_day = st.slider("Hour of Day (0-23)", 0, 23, 17, step=1)
 
 # Safety prioritizer is calculated to ensure the sum of percentages in population equals 1
 safe_pc = 1.0 - time_pc - money_pc
@@ -58,8 +68,9 @@ st.write(f"Time Prioritizer: {time_pc:.2f}, Money Prioritizer: {money_pc:.2f}, S
 population = Population(time_pc, money_pc, safe_pc)
 
 # populate neighborhoods table
-neighborhoods['cost_uber_all'] = average_uber_all_cost(Population(time_pc,money_pc,safe_pc), TransportCost(uber_all_time=1.25, uber_bart_mix_time=2, safety_cost=safety_cost, uber_cost_per_mile=uber_cost_per_mile, uber_distance=neighborhoods['distance_to_berryessa_miles']), time_to_money_conversion)
-neighborhoods['cost_bart_uber_mix'] = average_uber_bart_mix_cost(Population(time_pc,money_pc,safe_pc), TransportCost(uber_all_time=1.25, uber_bart_mix_time=2, safety_cost=safety_cost, uber_cost_per_mile=uber_cost_per_mile, uber_distance=neighborhoods['distance_to_berryessa_miles']), time_to_money_conversion) - subsidy
+time_scaling_factor = scaling_factor[hour_of_day]
+neighborhoods['cost_uber_all'] = average_uber_all_cost(Population(time_pc,money_pc,safe_pc), TransportCost(uber_all_time=1.25*time_scaling_factor, uber_bart_mix_time=2*time_scaling_factor, safety_cost=safety_cost, uber_cost_per_mile=uber_cost_per_mile, uber_distance=neighborhoods['distance_to_berryessa_miles']), time_to_money_conversion)
+neighborhoods['cost_bart_uber_mix'] = average_uber_bart_mix_cost(Population(time_pc,money_pc,safe_pc), TransportCost(uber_all_time=1.25*time_scaling_factor, uber_bart_mix_time=2*time_scaling_factor, safety_cost=safety_cost, uber_cost_per_mile=uber_cost_per_mile, uber_distance=neighborhoods['distance_to_berryessa_miles']), time_to_money_conversion) - subsidy
 neighborhoods['percent_choosing_bart_uber_mix'] = 100 * (neighborhoods['cost_uber_all'] - neighborhoods['cost_bart_uber_mix']) / neighborhoods['cost_uber_all']
 neighborhoods['percent_choosing_bart_uber_mix'] = neighborhoods['percent_choosing_bart_uber_mix'].clip(lower=0, upper=100)  # Cap values between 0 and 100%
 
@@ -78,4 +89,5 @@ for _, row in neighborhoods.iterrows():
         fill_opacity=0.7
     ).add_to(m)
 colormap.add_to(m)
+
 st_folium(m, width=700, height=500)
