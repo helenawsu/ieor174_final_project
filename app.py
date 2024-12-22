16c54e9371a4494ed639e094b1fdc24f72a9fdc3
Helena Su <helenasu@berkeley.edu>
2024-12-22 15:35:43 -0800

    add report

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ with left_col:
     col1, col2 = st.columns(2)
     with col1:
         subsidy = st.slider("Subsidy Amount ($)", 0.0, 8.0, 4.0, step=0.01)
-        time_to_money_conversion = st.slider("Time to Money Conversion ($/hour)", 0, 50, 25)
+        time_cost_rate= st.slider("Time to Cost Rate ($/hour)", 0, 50, 25)
         car_pc = st.slider("Percent of Population who own car (%)", 0.0, 1.0, 1 / 2, step=0.01)
     with col2:
         hour_of_day = st.slider("Hour of Day (5AM-21PM)", 5, 21, 17, step=1)
@@ -74,12 +74,12 @@ with left_col:
     else:
         neighborhoods["population"] = neighborhoods.POPTOTAL
 
-    ua = UberAll(time_to_money_conversion, neighborhoods['distance_to_berryessa_miles'])
-    ub = UberBartMix(time_to_money_conversion, neighborhoods['distance_to_berryessa_miles'], safety_cost)
+    ua = UberAll(time_cost_rate, neighborhoods['distance_to_berryessa_miles'])
+    ub = UberBartMix(time_cost_rate, neighborhoods['distance_to_berryessa_miles'], safety_cost)
     neighborhoods['cost_uber_all'] = ua.get_cost(hour_of_day, is_weekday)
     neighborhoods['cost_bart_uber'] = ub.get_cost() - subsidy
     neighborhoods['cost_bart_uber_no_subsidy'] = ub.get_cost()
-    neighborhoods['cost_drive'] = Drive(time_to_money_conversion, inconvenience_fee).get_cost(hour_of_day, is_weekday)
+    neighborhoods['cost_drive'] = Drive(time_cost_rate, inconvenience_fee).get_cost(hour_of_day, is_weekday)
 
     beta = 0.3
     exp_bart_uber_mix = np.exp(-beta * neighborhoods['cost_bart_uber'])
@@ -150,6 +150,8 @@ st.markdown("- Ridership with no subsidy is based on past 12 months (12/2023 - 1
 st.markdown("- Costs are converted to probability with the weighted average of two logistic functions of beta = 0.3, one carless (bart vs uber), one with car(bart vs uber vs drive).")
 st.markdown("- Revenue increased is calculated with the difference between the percentage of choosing BART with and without subsidy, where without subsidy is the past 12 months ridership count (Berryessa, Downtown Berkeley) * BART fare.")
 st.markdown("- Driving Inconvenience Cost includes parking, toll, and misc. cost such as driving in terms of energy.")
+st.markdown("- For more details, see [Written Report](https://docs.google.com/document/d/1ufr4oLwcIxWtQ4pYIvVG1Ovlq07B3Hdzpc68Vi2L9Cw/edit?usp=sharing).", unsafe_allow_html=True)
+
 
 
 st.write("### Debug")
